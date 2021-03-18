26cc4ef813d16da582c12d2167d2455d3c0ed361
4thePeace2 <milosmaljen@gmail.com>
2021-03-18 17:41:46 +0100

    Add project files.

---
+++ ZavrsniMilosMaljenovicNet11/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ZavrsniMilosMaljenovicNet11.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ ZavrsniMilosMaljenovicNet11/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ZavrsniMilosMaljenovicNet11.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
